572c3d6c344f367d810220a28d92474cdc96008a
Sergei Maiorov <ripinsurreality@gmail.com>
2020-10-17 16:54:52 +0300

    Updates

---
+++ precache-manifest.bd1c91941726e7019fac7a9720b921da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5470470921d696b396611ba230b6b1",
+    "url": "/dungeon-world-cb/index.html"
+  },
+  {
+    "revision": "f2467a92314c04753868",
+    "url": "/dungeon-world-cb/static/css/main.4b675743.chunk.css"
+  },
+  {
+    "revision": "514a8a275c49dc70d684",
+    "url": "/dungeon-world-cb/static/js/2.dd98daf7.chunk.js"
+  },
+  {
+    "revision": "b34d611d69419bc1205d1e94f9169aa7",
+    "url": "/dungeon-world-cb/static/js/2.dd98daf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2467a92314c04753868",
+    "url": "/dungeon-world-cb/static/js/main.0c8951a8.chunk.js"
+  },
+  {
+    "revision": "732d349e3742b5feb678",
+    "url": "/dungeon-world-cb/static/js/runtime-main.4f217bc6.js"
+  }
+]);
